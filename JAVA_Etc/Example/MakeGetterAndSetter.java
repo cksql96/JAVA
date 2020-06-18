12c997e9fe05563c42c1077a759c5ccb5e656ebe
cksql96 <66000771+cksql96@users.noreply.github.com>
2020-06-18 17:13:27 +0900

    Add files via upload

---
+++ JAVA_Etc/Example/MakeGetterAndSetter.java
@@ -0,0 +1,27 @@
+package example;
+
+public class MakeGetterAndSetter {
+
+	//ÇÊµå
+	private int speed;
+	private boolean stop;
+	
+	
+	//source - Generate Getter and Setter - click those blanks and generate it.
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public boolean isStop() {
+		return stop;
+	}
+	public void setStop(boolean stop) {
+		this.stop = stop;
+	}
+	
+	
+	
+	
+}
